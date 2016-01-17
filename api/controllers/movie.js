47b8796f062c617a5606f5472ccffa228472da39
Jeff Mills <jeffy9@gmail.com>
2016-01-17 18:31:41 -0500

    google search for movies

--- api/controllers/movie.js
+++ api/controllers/movie.js
@@ -1,15 +1,28 @@
+var google = require('google')
+google.resultsPerPage = 25;
+
 module.exports = function(kodi) {
 	return {
 		findByTitle: function(movieTitle, callback) {
 			var foundTitle = movieTitle;
 
-			kodi.VideoLibrary.GetMovies()
+			google('movie '+movieTitle, function (err, next, links){
+				console.log(links)
+			})
+			
+			kodi.VideoLibrary.GetMovies({'properties':['imdbnumber']})
 			.then(function(movies) {
-
+				//console.log(movies)
 				if(!(movies && movies.result && movies.result.movies && movies.result.movies.length > 0)) {
 				//	res.json({ message: 'I can\'t seem to retrieve a movie listing' });
 					callback("'I can't seem to retrieve a movie listing.")
 				}
+
+				var movie = movies.result.movies.reduce(function(result, item) {
+
+					//var label = item.label.toLowerCase().replace(/[&\/\\#,+\-()$~%.'":*?<>{}]/g, '')
+					//return result ? result : (movieTitle === label ? item : null);
+				}, null);
 /*
 				var movie = movies.result.movies.reduce(function(result, item) {
 					var label = item.label.toLowerCase().replace(/[&\/\\#,+\-()$~%.'":*?<>{}]/g, '')
