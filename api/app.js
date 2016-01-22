d73aa02b0bdf47f58b7e51d75d04c17beb1c5c33
Jeff Mills <jeffy9@gmail.com>
2016-01-22 17:18:04 -0500

    Environment set up

--- api/app.js
+++ api/app.js
@@ -1,16 +1,37 @@
-var express = require("express"),
+require('dotenv').config();
+
+var fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    express = require("express"),
     bodyParser = require('body-parser'),
-    Kodi = require('kodi-rpc'),
-    app = express(),
-    kodi = new Kodi('192.168.0.239', '80');
+    Kodi = require('kodi-rpc');
+
+var env = process.env.NODE_ENV || 'development';
+var kodi = new Kodi(process.env.KODI_IP, process.env.KODI_PORT);
+
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app, kodi);
 
-var nomo = require('node-monkey').start();
+if (env !== 'production') {
 
-var server = app.listen(3000, function () {
-	console.log("Listening on port %s...", server.address().port);
-});
+	require('node-monkey').start();
+	http.createServer(app).listen(3000,  function(){
+		console.log("Express server listening on port 3000");
+	});
+
+}else{
+
+	var httpsOptions  = {
+		key: fs.readFileSync(process.env.SSL_KEY),
+		cert: fs.readFileSync(process.env.SSL_CERT),
+	};
+
+	https.createServer(httpsOptions, app).listen(443, function(){
+		console.log("Express server listening on port 443");
+	});
+}
