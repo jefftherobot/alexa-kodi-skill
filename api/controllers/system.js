bb79f11fe6242854553fb82e2ba447b04984f6c0
Jeff Mills <jeffy9@gmail.com>
2016-01-25 16:17:14 -0500

    Working kodi actions

--- api/controllers/system.js
+++ api/controllers/system.js
@@ -1,27 +1,36 @@
 var wol = require('wake_on_lan');
 
 var actions = {
-	on : function(res){
+	on : function(res, kodi){
 		res.json({ message: 'Turning T.V. on' });
 		console.log('turning tv on...')
+		wake_kodi()
 	},
-	off: function(res){
+	off: function(res, kodi){
 		res.json({ message: 'Turning T.V. off' });
 		console.log('turning tv off...')
+		kodi.System.Suspend();
 	},
-	stop : function(res){
+	stop : function(res, kodi){
 		res.json({ message: 'Stopping playback' });
+		kodi.Player.Stop({'playerid':1});
 		console.log('stopping.')
 	},
+}
+
+
+function wake_kodi(){
+	wol.wake('80:EE:73:63:F0:5A',{ address: '192.168.0.255'});
 }
 
 
 module.exports = function(kodi) {
 	return {
 		action: function(req, res) {
+			console.log(kodi)
 			var action = req.params.action;
 			if(actions[action]){
-				actions[action](res);
+				actions[action](res, kodi);
 			}else{
 				res.json({ message: 'System command not found' });
 			}
